9ad3a1596030eabfb62cc75ada6eb68bc1cc64c0
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-17 20:00:59 +0700

    model

---
+++ models/Singer.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mean1812', { useMongoClient: true });
+
+const singerSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    link: { type: String, required: true, unique: true },
+    image: { type: String, unique: true },
+});
+
+const Singer = mongoose.model('Singer', singerSchema);
+
+module.exports = Singer;
